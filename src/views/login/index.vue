846f1dc68ed6d236583b545ba48a98c668e16cf1
jessie717 <jessiezly717@gmail.com>
2024-07-05 11:33:28 +0800

    feat: 修改登录页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="bg-gray-2 w-full h-full flex justify-center items-center bg-[url('/imgs/登录页背景.png')]">
+  <div
+    class="bg-gray-2 w-full h-full flex justify-center items-center bg-[url('/src/assets/images/login-bg.png')] relative"
+  >
+    <Logo class="absolute top-4 left-9" />
     <div
-      class="w-[800px] h-[500px] rounded-[18px] shadow-[0px_6px_27px_0px_rgba(0,0,0,0.1)] bg-white flex justify-center items-center">
-      <img src="/imgs/登录页主图.png" class="w-1/2 h-full" />
+      class="w-[800px] h-[500px] rounded-[18px] shadow-[0px_6px_27px_0px_rgba(0,0,0,0.1)] bg-white flex justify-center items-center"
+    >
+      <img src="/src/assets/images/login-info.png" class="w-1/2 h-full" />
       <div class="w-1/2 h-full">
         <LoginForm />
       </div>
@@ -11,8 +15,7 @@
 </template>
 
 <script setup lang="ts">
-import LoginForm from './LoginForm.vue';
-
+import LoginForm from './LoginForm.vue'
 </script>
 
 <style scoped></style>
