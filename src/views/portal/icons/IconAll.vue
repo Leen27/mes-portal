e3df0e5960bed5bb501f766135381b1019689b1b
jessie717 <jessiezly717@gmail.com>
2024-07-08 16:32:15 +0800

    feat: 新增icon组件

---
+++ src/views/portal/icons/IconAll.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg width="30" height="30" viewBox="0 0 30 30" shape-rendering="geometricPrecision"
+    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <g id="frame-yhW2-1q0g/yhW2-1i6t">
+      <path d="M0 0L30 0L30 30L0 30L0 0Z" shape-rendering="geometricPrecision" fill="#4747FF" style="fill-rule:evenodd"
+        transform="matrix(1 0 0 1 0 0)"></path>
+      <path
+        d="M0 2C0 0.895431 0.895431 0 2 0L5 0C6.10457 0 7 0.895431 7 2L7 5C7 6.10457 6.10457 7 5 7L2 7C0.895431 7 0 6.10457 0 5L0 2ZM0 10C0 8.89543 0.895431 8 2 8L5 8C6.10457 8 7 8.89543 7 10L7 13C7 14.1046 6.10457 15 5 15L2 15C0.895431 15 0 14.1046 0 13L0 10ZM10 8C8.89543 8 8 8.89543 8 10L8 13C8 14.1046 8.89543 15 10 15L13 15C14.1046 15 15 14.1046 15 13L15 10C15 8.89543 14.1046 8 13 8L10 8Z"
+        shape-rendering="geometricPrecision" fill="#FFFFFF" style="fill-rule:evenodd"
+        transform="matrix(0.7071067811865476 0.7071067811865475 -0.7071067811865475 0.7071067811865476 15 4.300000188024569)">
+      </path>
+      <path
+        d="M0 2C0 0.895431 0.895431 0 2 0L5 0C6.10457 0 7 0.895431 7 2L7 5C7 6.10457 6.10457 7 5 7L2 7C0.895431 7 0 6.10457 0 5L0 2Z"
+        shape-rendering="geometricPrecision" fill="#01C59D" style="fill-rule:evenodd"
+        transform="matrix(0.7071067811865476 0.7071067811865475 -0.7071067811865475 0.7071067811865476 20.65673828125 9.95684776179769)">
+      </path>
+    </g>
+  </svg>
+</template>
