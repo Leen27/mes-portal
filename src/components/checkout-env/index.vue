384b31e8db8e2c81d3e713b501e07b96806abd39
jessie717 <jessiezly717@gmail.com>
2024-07-09 15:23:51 +0800

    feat: 补充菜单栏逻辑

--- src/components/checkout-env/index.vue
+++ src/components/checkout-env/index.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="max-h-[500px] overflow-y-auto mt-[30px]">
-    <div v-for="(env, envIndex) in list" :key="env.title" class="pb-[20px]">
+    <div v-for="env in envs" :key="env.envId" class="pb-[20px] cursor-pointer">
       <div :class="['w-52 h-12 flex items-center justify-center gap-4']">
         <IconTestEnv />
-        <span class="w-40">{{ env.title }}</span>
+        <span class="w-40">{{ env.envName }}</span>
       </div>
-      <div v-for="(item, itemIndex) in env.data" :key="item.name" :class="getItemStyle(`${envIndex}-${itemIndex}`)"
-        @click="setActiveId(`${envIndex}-${itemIndex}`)">
+      <div
+        v-for="sub in env.subEnvs"
+        :key="sub.subEnvId"
+        :class="getItemStyle(sub.subEnvId)"
+        @click="setActiveId(sub.subEnvId)"
+      >
         <div class="flex-1 flex items-center justify-center">
-          <div name="dot" :class="getDotStyle(`${envIndex}-${itemIndex}`)"></div>
+          <div name="dot" :class="getDotStyle(sub.subEnvId)"></div>
         </div>
         <div class="flex flex-col justify-center w-40">
-          <span class="text-3 leading-[21px]">{{ item.name }}</span>
-          <span class="text-1 text-gray-env leading-[18px]">{{ item.ip }}</span>
+          <span class="text-3 leading-[21px]">{{ sub.subEnvName }}</span>
+          <span class="text-1 text-gray-env leading-[18px]">{{ sub.remark }}</span>
         </div>
       </div>
     </div>
@@ -20,61 +24,55 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { onMounted, ref } from 'vue'
 import { IconTestEnv } from './icons'
+
+import { getUserApps, getUserEnvs } from '@/http/user'
+import { useUserStore } from '@/stores/user'
+
+import type { EnvItem } from '#/user'
+
+const userStore = useUserStore()
 
 const currentActiveId = ref('')
 
-function setActiveId(id: string) {
+async function setActiveId(id: string) {
+  if (id === currentActiveId.value) {
+    return
+  }
   currentActiveId.value = id
 }
 
 function getItemStyle(id: string) {
-  return ['w-52 h-12 flex items-center justify-center cursor-pointer rounded gap-4 px-4 py-2', {
-    'bg-white': id == currentActiveId.value,
-    'shadow-[0px_6px_27px_0px_rgba(0,0,0,0.1)]': id == currentActiveId.value,
-  }]
+  return [
+    'w-52 h-12 flex items-center justify-center cursor-pointer rounded gap-4 px-4 py-2',
+    {
+      'bg-white': id == currentActiveId.value,
+      'shadow-[0px_6px_27px_0px_rgba(0,0,0,0.1)]': id == currentActiveId.value
+    }
+  ]
 }
 
 function getDotStyle(id: string) {
-  return [{
-    'bg-primary': id === currentActiveId.value,
-    'bg-gray-env': id !== currentActiveId.value,
-  }, 'rounded-full  w-3 h-3']
+  return [
+    {
+      'bg-primary': id === currentActiveId.value,
+      'bg-gray-env': id !== currentActiveId.value
+    },
+    'rounded-full  w-3 h-3'
+  ]
 }
-const list = ref([
-  {
-    title: '测试环境',
-    data: [
-      {
-        name: '产品',
-        ip: '192.168.1.100',
-      },
-      {
-        name: 'STIC',
-        ip: '192.168.1.101',
-      },
-      {
-        name: 'Naura',
-        ip: '192.168.1.102',
-      },
-      {
-        name: 'Sidtek',
-        ip: '192.168.1.103',
-      },
-    ]
-  },
-  {
-    title: 'Demo环境',
-    data: [
-      {
-        name: '产品',
-        ip: '192.168.1.104',
-      },
-    ]
-  }
-])
 
+const envs = ref<Array<EnvItem>>([])
+
+onMounted(async () => {
+  const userEnvs = await getUserEnvs()
+  envs.value = userEnvs
+  currentActiveId.value = userEnvs[0].envId // 初始化env
+  const apps = await getUserApps(currentActiveId.value)
+  userStore.setFavourApps(apps.favourApps)
+  userStore.setAppGroups(apps.appGroups)
+})
 </script>
 
 <style scoped></style>
